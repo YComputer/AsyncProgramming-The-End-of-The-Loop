94dcc980f307015a94cdfc6b3f634b45771de907
YComputer <thea@163.com>
2016-09-05 16:22:40 +0800

    array chain invoke

---
+++ arrayFilter.js
@@ -0,0 +1,13 @@
+var stocks = [
+  { symbol: "XFX", price: 240.22, volume: 23432 },
+  { symbol: "TNZ", price: 332.19, volume: 234 },
+  { symbol: "JXJ", price: 120.22, volume: 5323 },
+];
+
+var filteredStockSymbols = stocks.filter(function(stock){
+    return stock.price>150;
+}).map(function(stock){
+    return stock.symbol;
+})
+
+console.log(filteredStockSymbols);
